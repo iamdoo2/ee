7b3987fb9447e827bf762749baedab20be295601
Duhee Lee <doo2@wafflestudio.com>
2013-04-21 00:53:17 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asdf::Application.config.secret_token = 'd2cc7a7d83e54f5e226e01eec9cf090efd4243bfecbba23a18eb3fb7ba44cc7d2d47a30ec8054af9e1289aafe10ba541553d86a9da3ebd269dc253afad06b0da'
